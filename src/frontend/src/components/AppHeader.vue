642e80d7353ba76ef8ae03692ded174787858db7
myrrhax <sizintsevdenis05@mail.ru>
2023-03-18 21:07:31 +0300

    test

--- src/frontend/src/components/AppHeader.vue
+++ src/frontend/src/components/AppHeader.vue
@@ -1,21 +1,37 @@
 <template>
-  <div>
-    <button @click='login'>Войти</button>
+  <div class='wrapper' style="width: 100%;height: 50px;background-color: green;">
+    
+    <div class='auth' v-if='!currentUser'>
+        <button @click='login'>Войти</button>
+        <button @click='registration'>Регистрация</button>
+    </div>
+    <div v-else>
+        <button @click='logout'>Выйти</button>
+    </div>
   </div>
-  <div>Регистрация</div>
 </template>
 
 <script>
 export default {
     name: 'AppHeader',
+    props: {
+        showAuth: Boolean
+    },
+    computed:{
+        currentUser() {
+            console.log(this.$store.state.auth.user )
+            return this.$store.state.auth.user 
+        }
+    },
     methods: {
         async login() {
-            console.log('sfamfmsamf')
-            let data = await this.$store.dispatch('auth/login', {
-                name: 'myrrhax',
-                password: '17983'
-            })
-            console.log(data)
+            
+        },
+        async registration() {
+            
+        },
+        async logout(){
+            this.$store.dispatch('auth/logout')
         }
     }
 }
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class='container'>
-    <div v-if='currentUser'>{{ currentUser }}</div>
-    <div v-else>НЕ ЗАРЕГАН</div>
+    HELLO WORLD!
   </div>
 </template>
 
